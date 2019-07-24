c4881c370ba7b353f8e6e9490efaeb8aec56bf3e
Rob <18713128+CaptainSloth@users.noreply.github.com>
2019-07-24 06:04:43 -0700

    Add files via upload

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distancePerFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distancePerFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distancePerFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        Debug.Log("HIT");
+    }
+}
--- Assets/Scripts/EnemyTurret.cs
+++ Assets/Scripts/EnemyTurret.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class EnemyTurret : MonoBehaviour
 {
-    Transform target;
+    private Transform target;
+
+    [Header("Attributes")]
     public float range = 15f;
+    public float fireRate = 1f; //1f = 1 shot per sec
+    private float fireCountdown = 0f;
+
+    [Header("Settings")]
     public float turnSpeed = 10f;
     public Transform partToRotate;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Start()
     {
         target = PlayerManager.instance.player.transform; //Pulls "Player" out of PlayerManager
-        InvokeRepeating("UpdateTarget", 0f, 0.5f); //Calls UpdateTarget 2x 1sec
+        // InvokeRepeating("UpdateTarget", 0f, 0.5f); //Calls UpdateTarget 2x 1sec
     }
 
-    void UpdateTarget()
-    {
-        float distanceToPlayer = Vector3.Distance(target.position, transform.position);
-        if (distanceToPlayer <= range)
-        {
-            Debug.Log("Yo mofo I've found:" + target);
-        }
-    }
+    //void UpdateTarget()
+    //{
+    //    float distanceToPlayer = Vector3.Distance(target.position, transform.position);
+    //    if (distanceToPlayer <= range)
+    //    {
+    //        Debug.Log("Yo mofo I've found:" + target);
+    //    }
+    //}
+    
 
     void Update()
     {
@@ -31,11 +40,31 @@ public class EnemyTurret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir); //Look Direction
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; //Converts to eulerangles
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f); //Sets rotation on y
+
+        //Shooting
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
     }
 
-    private void OnDrawGizmosSelected()
+    void Shoot()
+    {
+        Debug.Log("Turret go PEW!");
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<bullet>();
+
+        if (bullet != null)
+            Bullet.Seek(target);
+    }
+
+private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(transform.position, range);
     }
 }
+
